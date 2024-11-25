51f8ec24eda0a555b097939bf2c55ed2792083a4
httpiego <belardinelli.diego@gmail.com>
2024-11-25 18:58:43 +0100

    everything working

--- model2.py
+++ model2.py
@@ -15,7 +15,7 @@ import random
 # Hyperparameters
 sequence_length = 2618  #295 average / #2618 longest
 embedding_dim = 64
-rnn_units = 64
+rnn_units = 32
 k_value = 5
 num_classes = 1
 batch_size = 128  # 500
@@ -27,12 +27,19 @@ def build_vuldee_model(sequence_length, embedding_dim, rnn_units, k_value, num_c
     inputs = layers.Input(shape=(sequence_length, embedding_dim), name='input_layer')
 
     #brnn = layers.Bidirectional(layers.LSTM(rnn_units, return_sequences=True), name='bidirectional_rnn')(inputs)
-    gru = layers.Bidirectional(layers.GRU(rnn_units, return_sequences=True), name='bidirectional_rnn')(inputs)
+    gru = layers.Bidirectional(layers.GRU(rnn_units,
+                                          return_sequences=True,
+                                          kernel_regularizer=regularizers.l2(0.01),
+                                          recurrent_regularizer=regularizers.l2(0.01),),
+                               name='bidirectional_rnn')(inputs)
 
-    dense = layers.Dense(rnn_units, kernel_regularizer=regularizers.l1_l2(l1=0.01, l2=0.01), activation='relu', name='dense_layer')(gru)
+    dense = layers.Dense(rnn_units,
+                         kernel_regularizer=regularizers.l1_l2(l1=0.01, l2=0.01),
+                         activation='relu', name='dense_layer')(gru)
+
     #dense = layers.Dense(rnn_units, kernel_regularizer=regularizers.l2(0.001), activation='relu', name='dense_layer')(gru)
 
-    dropout = layers.Dropout(0.3)(dense) #
+    dropout = layers.Dropout(0.5)(dense) #
 
     batch = BatchNormalization()(dropout)  #
 
@@ -60,8 +67,8 @@ def build_vuldee_model(sequence_length, embedding_dim, rnn_units, k_value, num_c
 
 vuldee_model = build_vuldee_model(sequence_length, embedding_dim, rnn_units, k_value, num_classes)
 
-vuldee_model.compile(optimizer=Adam(learning_rate=0.001, clipnorm=1.0), loss='binary_crossentropy', metrics=['accuracy'])
-#vuldee_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#vuldee_model.compile(optimizer=Adam(learning_rate=0.001, clipnorm=1.0), loss='binary_crossentropy', metrics=['accuracy'])
+vuldee_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 
 vuldee_model.summary()
@@ -70,6 +77,31 @@ vuldee_model.summary()
 train_folder = '/home/httpiego/PycharmProjects/VulDeeDiegator/iSeVCs/Vectorized/Training/'
 test_folder = '/home/httpiego/PycharmProjects/VulDeeDiegator/iSeVCs/Vectorized/Testing/'
 
+
+def plot(all_losses, all_accuracies,  all_test_losses, all_test_accuracies, train_num, plot_num):
+    plt.figure(figsize=(12, 6))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(all_losses, label='Training Loss', color='blue')
+    plt.plot(all_test_losses, label='Testing Loss', color='orange')
+    plt.title('Loss Over Time')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(all_accuracies, label='Training Accuracy', color='blue')
+    plt.plot(all_test_accuracies, label='Testing Accuracy', color='orange')
+    plt.title('Accuracy Over Time')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+
+    plt.savefig(f'/home/httpiego/PycharmProjects/VulDeeDiegator/trained_models/1/training_progress_{plot_num}.png')
 
 def load_data_train(file_name):
     data = np.load(train_folder + file_name, allow_pickle=True)
@@ -142,39 +174,54 @@ all_accuracies = []
 #TRAIN
 
 train_files_lenght = len(os.listdir(train_folder))
+print('ciaooo ' + str(train_files_lenght))
 train_files = os.listdir(train_folder)
 random.shuffle(train_files)
 start_index = 0
 last_index = batch_size
-while True:
-    files_in_batch = []
-    print(f'batch: {start_index} - {last_index}')
-    for j in range(start_index, last_index):
-        files_in_batch.append(train_files[j])
-    iSeVCs, vulnLocMatrixes, labels = create_batch_train(files_in_batch)
 
-        #PROVA 2000/4000 EPOCHE - TOGLIERE EARLY STOPPING - TRAINING E VALIDAZIONE CLASSICHE
-    tf.keras.backend.clear_session()
-    print(f'start index - {start_index}\n')
-    print(f'last index - {last_index}\n')
-    history = vuldee_model.fit([iSeVCs, vulnLocMatrixes], labels,
-                                   epochs=250, batch_size=batch_size,
-                                   #callbacks=[early_stopping],
-                                   callbacks=[early_stopping, reduce_lr],
-                                   validation_split=0.2
-                                   )
+train_num = 1
 
-    all_losses.extend(history.history['loss'])
-    all_accuracies.extend(history.history['accuracy'])
+newpath = f'/home/httpiego/PycharmProjects/VulDeeDiegator/trained_models/{train_num}'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
 
-    del files_in_batch, iSeVCs, vulnLocMatrixes, labels
-    gc.collect()
-    start_index = last_index
-    if start_index == train_files_lenght-1:
-        break
-    last_index += batch_size
-    if last_index > (train_files_lenght-1):
-        last_index = train_files_lenght-1
+check = True
+for i in range(100):
+    while True:
+        if start_index == train_files_lenght:
+            check = False
+            break
+        files_in_batch = []
+        print(f'batch: {start_index} - {last_index}')
+        for j in range(start_index, last_index):
+            files_in_batch.append(train_files[j])
+        iSeVCs, vulnLocMatrixes, labels = create_batch_train(files_in_batch)
+
+            #PROVA 2000/4000 EPOCHE - TOGLIERE EARLY STOPPING - TRAINING E VALIDAZIONE CLASSICHE
+        tf.keras.backend.clear_session()
+        print(f'start index - {start_index}')
+        print(f'last index - {last_index}')
+        history = vuldee_model.fit([iSeVCs, vulnLocMatrixes], labels,
+                                       epochs=1, batch_size=batch_size,
+                                       #callbacks=[early_stopping],
+                                       #callbacks=[early_stopping, reduce_lr],
+                                       #validation_split=0.2
+                                       )
+
+        all_losses.extend(history.history['loss'])
+        all_accuracies.extend(history.history['accuracy'])
+
+        del files_in_batch, iSeVCs, vulnLocMatrixes, labels
+        gc.collect()
+        start_index = last_index
+        last_index += batch_size
+        if last_index > train_files_lenght:
+            last_index = train_files_lenght
+    print('last for loop index --> ' + str(i))
+    check = True
+    start_index = 0
+    last_index = batch_size
 
 
 #EVALUATE
@@ -222,40 +269,21 @@ while True:
         last_index = test_files_lenght - 1
 
 
-vuldee_model.save('/home/httpiego/PycharmProjects/VulDeeDiegator/trained_models/trained_model_7.keras')
+    average_loss = np.mean(all_test_losses)
+    average_accuracy = np.mean(all_test_accuracies)
+
+    print('==========================================================')
+    print('======================= EVALUATE =========================')
+    print('==========================================================')
+    print(f'Average Test Loss: {average_loss:.4f}')
+    print(f'Average Test Accuracy: {average_accuracy:.4f}')
+    print('==========================================================')
+    print('==========================================================')
+    print('==========================================================')
 
 
-average_loss = np.mean(all_test_losses)
-average_accuracy = np.mean(all_test_accuracies)
+    # Plot the results after training
+    plot(all_losses, all_accuracies, all_test_losses, all_test_accuracies, train_num, train_num)
 
-print('==========================================================')
-print('======================= EVALUATE =========================')
-print('==========================================================')
-print(f'Average Test Loss: {average_loss:.4f}')
-print(f'Average Test Accuracy: {average_accuracy:.4f}')
-print('==========================================================')
-print('==========================================================')
-print('==========================================================')
-
-
-# Plot the results after training
-plt.figure(figsize=(12, 6))
-
-# Plot loss
-plt.subplot(1, 2, 1)
-plt.plot(all_losses, label='Training Loss')
-plt.title('Training Loss Over Time')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.subplot(1, 2, 2)
-plt.plot(all_accuracies, label='Training Accuracy')
-plt.title('Training Accuracy Over Time')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-
-plt.savefig(f'/home/httpiego/PycharmProjects/VulDeeDiegator/trained_models/graphes/training_progress_7.png')
-
+vuldee_model.save(f'/home/httpiego/PycharmProjects/VulDeeDiegator/trained_models/{train_num}/trained_model_{train_num}.keras')
 
